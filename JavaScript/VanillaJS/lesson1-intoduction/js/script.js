c473749d80a2cfdebdee8b04a68285d59b91002c
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-08-05 12:30:49 +0400

    js lesson1

---
+++ JavaScript/VanillaJS/lesson1-intoduction/js/script.js
@@ -0,0 +1,18 @@
+// one line comment
+/**
+ * more
+ * line 
+ * comment
+ */
+// document.write("Hello JS");
+
+// debugger
+// console.log("Hello1");
+// console.log("Hello2");
+// console.log("Hello3");
+// console.log("Hello4");
+// console.log("Hello5");
+
+// alert("hello");
+// window.alert("hello");
+
---
+++ JavaScript/VanillaJS/lesson2-datatypes/js/script.js
@@ -0,0 +1,44 @@
+// var a = 5;
+// console.log(a);
+
+// Primitive Data Types 
+// string = metn 
+// var info = "Hello ";
+// var infoOne = "JS";
+// console.log(info+infoOne);
+
+// number = reqem
+// var info = 100;
+// var infoOne = 5;
+// console.log(info+infoOne);
+
+// boolean = mentiqi
+// true
+// false
+// if (true) {
+//     console.log("Duzdu");
+// }else{
+//     console.log("Sehvdi");
+// }
+
+// Composite Data Types
+// array
+// var infoArr = ["Pen",'Table',5,true];
+// console.log(infoArr);
+
+// object
+// var infoObj = {
+//     name: "Ismayil",
+//     surname: "Ismayilov",
+//     age:21
+// }
+// console.log(infoObj.age);
+
+// Special Data Types
+// undefined
+// var a = undefined;
+// console.log(a);
+
+// null
+// var b = null;
+// console.log(b);
