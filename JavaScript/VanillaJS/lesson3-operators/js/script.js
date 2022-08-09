8957d78ce666d4b2a21ae8eda3d32557accb2a33
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-08-09 12:26:18 +0400

    operators part1

---
+++ JavaScript/VanillaJS/lesson3-operators/js/script.js
@@ -0,0 +1,75 @@
+// OPERATORS
+// Assignment Operators (Daxil etme operatorlari)
+// =
+// +=
+// -=
+// *=
+// /=
+// %=
+// **=
+
+// Arithmetic Operators (Riyazi operatorlar)
+// +
+// -
+// *
+// /
+// %
+// **
+// ++
+// --
+
+// Comparison Operators (Muqayise operatorlari)
+
+// ==
+// ===
+// >
+// >=
+// <
+// <=
+// !=
+// !==
+
+// var a = "Hello";
+// var b = "Mello";
+// if (b > a) {
+//     console.log("Bu muqayise duzgundur");
+// }else{
+//     console.log("Bu muqayise sehvdir");
+
+// }
+
+
+// Logical Operators (Mentiqi operatorlar)
+// && - AND (ve)
+// || - OR  (ve ya)
+// !  - NOT (deyil)
+
+// if ((5 === "5") ||('a'<'b') && false) {
+//     console.log("Bu muqayise duzgundur");
+// }else{
+//     console.log("Bu muqayise sehvdir");
+
+// }
+
+
+// const usernamedb = "ismayil2001";
+// const passworddb = 'ismayil.123';
+
+// document.querySelector('button').onclick = ()=>{
+//     if ((document.querySelector('#un').value === usernamedb) && (document.querySelector('#pass').value === passworddb)) {
+//             document.querySelector('#result').innerHTML = "Login successfull...";
+//             document.querySelector('#result').style.color = "green";
+//     }else{
+//         document.querySelector('#result').innerHTML = "password or username is wrong...";
+//         document.querySelector('#result').style.color = "red";
+//     }
+// }
+
+
+// String Operators (Metn operatorlar)
+// +
+// +=
+// var a = "Java";
+// var b = "Script";
+// a+=b;
+// document.write(a);
