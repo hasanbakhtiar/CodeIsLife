95d5781bcd322b79456a26f5b3093c5ab0d5f517
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-08-16 12:37:43 +0400

    loop array object

---
+++ JavaScript/VanillaJS/lesson5-loops/js/script.js
@@ -0,0 +1,52 @@
+// Loops
+// while
+// let count = 0;
+// while(count <= 100){
+//     // if (count === 5) {
+//         // break;
+//     // }
+//     console.log(count);
+//     count++;
+//     // count+=1;
+// }
+
+// let count = 100;
+// while (count >= 101) {
+//     console.log("JS"+count);
+//     count--;
+// }
+
+
+// do while
+// let count = 101;
+// do{
+//     console.log(count);
+//     count++;
+// }while(count<=100);
+
+// for 
+// for(let count = 0; count <= 10; count++){
+//   if (count ===2) {
+//     continue;
+//   }
+//   console.log(count);
+// }
+
+// let myFruits = ['apple','banan','pear','peach','grape','cerry'];
+// console.log(myFruits.length);
+// for(let index = 0; index < myFruits.length; index++){
+//     console.log(myFruits[index]);
+// }
+// for in
+// let myFruits = ['apple','banan','pear','peach','grape','cerry'];
+// for(x in myFruits){
+//     console.log(x+"-"+myFruits[x]);
+    
+// }
+
+// for of 
+// let myFruits = ['apple','banan','pear','peach','grape','cerry'];
+
+// for(x of myFruits){
+//     console.log(x);
+// }
---
+++ JavaScript/VanillaJS/lesson6-arrayobject/js/script.js
@@ -0,0 +1,48 @@
+// let myArr = new Array("pen",'table','book');
+
+// let myArr = ['pen','table','book'];
+// let myArr = [];
+// myArr[0] = 'pen';
+// myArr[1] = 'book';
+
+// let myArr = ['pen','table','book'];
+// myArr[1]='car';
+
+// let myArr = ['pen','table','book'];
+// myArr[5]='new value';
+
+// let myArr = ['pen1','table','book'];
+// myArr['infopen'] = 'pen2';
+
+// console.log(myArr['infopen']);
+
+// let myArr = ['pen',[1,2,3,4,['snow','rain'],true,['phone','telephone',['flag','ritm',['jump',['target'],'run']],'laptop']]];
+// console.log(myArr[1][6][2][2][1][0]);
+
+// Object
+// let car = [
+//   {
+//     brand: "BMW",
+//     model: "X5",
+//     color: "white",
+//     year: 2022,
+//     newcar: true,
+//   },
+
+//   {
+//     brand: "Mercedes",
+//     model: "S500",
+//     color: "black",
+//     year: 2020,
+//     newcar: false,
+//   },
+// ];
+
+// delete car.newcar;
+// car.newcar = false;
+
+// for( x of car){
+//     console.log(x.brand);
+//     console.log(x.year);
+// }
+// console.log(car[0].brand);
