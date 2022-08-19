4bc614dceceef740a584ceedc701ea73989fbb1d
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-08-19 12:52:29 +0400

    function

---
+++ JavaScript/VanillaJS/lesson7-function/js/script.js
@@ -0,0 +1,78 @@
+// ES5
+
+// function info(){
+//     console.log('hello my first function');
+// }
+// const info = function(){
+//     console.log('hello my first function');
+// }
+
+// (function info(){
+//     console.log('hello my first function');
+// })()
+
+// function name(params){
+//     action
+// }
+
+// function infoCalc(a,b){
+//     return a+b;
+// }
+
+// console.log(infoCalc(5,10));
+
+// function infoCalc(a,b){
+//     console.log(a+b);
+// }
+
+// infoCalc(10,5);
+
+
+// function infoCalc(a=20,b=5,c=2){
+//     return a+b/c;
+// }
+// console.log(infoCalc(10,20,5));
+
+// ES6
+// normal function
+// function(){}
+// arrow function
+// ()=>{}
+// const info =(a,b)=>{
+//     return a,b;
+// }
+// console.log(info(10,20));
+
+
+// const car={
+//     name:'bmw',
+//     color:'yellow',
+//     speed:(s,t)=>{
+//         return s/t;
+//     }
+// }
+
+// console.log(car.name+" speed:"+car.speed(100,2)+"km/h");
+
+
+// let week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+
+
+// console.log(week);   
+
+
+// const myWeek =(calldata)=>{
+//     for(x of calldata){
+//         console.log(x);
+//     }
+// }
+// myWeek(week);
+
+
+
+
+
+
+
+
