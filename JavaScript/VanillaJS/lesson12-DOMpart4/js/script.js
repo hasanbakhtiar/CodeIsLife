d71999d44ac4f229ef5f4bc762ca7155be1c363c
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-09-09 13:19:18 +0400

    node

---
+++ JavaScript/VanillaJS/lesson12-DOMpart4/js/script.js
@@ -0,0 +1,78 @@
+const myinput = document.querySelector('input');
+// // myinput.onkeypress = ()=>{
+// //     myinput.style.backgroundColor = 'red';
+// // }
+// myinput.onkeyup = ()=>{
+//     myinput.style.backgroundColor = 'white';
+//     myinput.style.transition = '1s';
+//     myinput.style.height = '20px';
+
+// }
+
+// myinput.onkeydown = ()=>{
+//     myinput.style.backgroundColor = 'red';
+//     myinput.style.height = '70px';
+//     myinput.style.transition = '1s';
+
+// }
+
+// const btn = document.querySelector('button');
+// btn.onclick = ()=>{
+//     const li = document.createElement('li');
+//     li.innerHTML = myinput.value;
+//     const list = document.querySelector('#list');
+//     list.appendChild(li);
+//     myinput.value = "";
+//     li.onclick=()=>{
+//         li.style.textDecoration = 'line-through';
+//     }
+//     li.ondblclick=()=>{
+//         li.remove();
+//     }
+// }
+// btn.accessKey = 'w';
+
+// document.querySelector('p').innerHTML = document.querySelector('ul').firstChild.firstChild.nodeName;
+// document.querySelector('p').innerHTML = document.querySelector('ul').lastChild.firstChild.nodeValue;
+// document.querySelector('p').innerHTML = document.querySelector('ul').childNodes[2].firstChild.nodeValue;
+// document.querySelector('p').innerHTML = document.querySelector('ul').childNodes[1].nextSibling.firstChild.nodeValue;
+// document.querySelector('p').innerHTML = document.querySelector('li').parentNode.nodeName;
+// document.querySelector('p').innerHTML = document.querySelector('li').nextSibling.nodeName;
+// document.querySelector('p').innerHTML = document.querySelectorAll('li')[1].previousSibling.nodeName;
+
+// const text = document.createTextNode('hello everyone');
+// const p = document.querySelector('p');
+// p.appendChild(text);
+
+
+
+// const text = document.createTextNode('hello everyone');
+// const p = document.createElement('p');
+// p.appendChild(text);
+// document.body.appendChild(p);
+
+
+
+const data = [
+    {
+        name:"bmw",
+        color:'red'
+    },
+    {
+        name:"audi",
+        color:'black' 
+    }
+]
+
+// const ul = document.createElement('ul');
+// const li = document.createElement('li');
+// ul.appendChild(li);
+// let li=document.querySelectorAll('li');
+// data.map((fetchdata)=>{
+//     li.innerHTML = fetchdata.name;
+// })
+
+// for( x in data){
+//     li[x].innerHTML = data[x].name;
+// }
+// document.body.appendChild(ul);
