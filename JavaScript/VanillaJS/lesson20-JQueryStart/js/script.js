fb27f288643ca291e8f3e98136706c498214de77
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-09-27 12:42:21 +0400

    jquery enter

---
+++ JavaScript/VanillaJS/lesson20-JQueryStart/js/script.js
@@ -0,0 +1,53 @@
+// $(function(){
+//     $('ne ile').nece(function(){
+//         $('neyi').etsin();
+//     })
+// })
+
+// $(function(){
+//     $('button').click(function(){
+//         $('h1').hide();
+//     })
+// })
+
+
+// $(function(){
+//     $('#btn').click(function(){
+//         $('.big-text').show(1000);
+//     })
+// })
+
+$(function(){
+    $('#btn').click(function(){
+        // $('.big-text').fadeIn(2000);
+        // $('.big-text').fadeOut(2000);
+        // $('.big-text').fadeToggle(2000);
+        // $('.big-text').fadeTo(2000,.5);
+        // $('.big-text').slideUp(2000);
+        // $('.big-text').slideToggle(2000);
+        // $('.big-text').css("color",'green');
+    //     $('.big-text').css({
+    //         "color":'green',
+    //     'font-size':'50px',
+    //     'transition':'1s'
+    // },);
+
+    // $('.big-text').hide(1000,()=>{
+    //     document.querySelector('button').innerHTML = 'perfect';
+    //     // alert('working...')
+    // })
+    // $('.big-text').css("color", "green").slideUp(2000).slideDown(2000);
+    // $('.big-text').addClass('blue');
+    // $('.big-text').removeClass('blue');
+    // $('.big-text').toggleClass('blue');
+        // $('.big-text').animate({
+        //     left: '250px',
+        //     opacity: '0.5',
+        //     height: 'toggle',
+        //     width: '150px',
+        //   });
+    })
+})
+
+
+
