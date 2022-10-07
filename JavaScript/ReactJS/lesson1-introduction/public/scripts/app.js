7a937fdb3dc9841320f2b19140e0bbf9b0cf4229
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-10-07 12:40:18 +0400

    react intro

---
+++ JavaScript/ReactJS/lesson1-introduction/public/scripts/app.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var temp = /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", {
+  style: {
+    "color": "red"
+  }
+}, "Hello"), /*#__PURE__*/React.createElement("p", {
+  className: "a"
+}, "Lorem, ipsum dolor."), /*#__PURE__*/React.createElement("img", {
+  src: "https://www.gardendesign.com/pictures/images/675x529Max/site_3/helianthus-yellow-flower-pixabay_11863.jpg",
+  alt: ""
+}));
+ReactDOM.render(temp, document.querySelector('#root'));
---
+++ JavaScript/ReactJS/lesson1-introduction/src/index.js
@@ -0,0 +1,9 @@
+var temp = (
+    <>
+    <h1 style={{"color":"red"}}>Hello</h1>
+    <p className="a">Lorem, ipsum dolor.</p>
+    <img src="https://www.gardendesign.com/pictures/images/675x529Max/site_3/helianthus-yellow-flower-pixabay_11863.jpg" alt="" />
+    </>
+)
+
+ReactDOM.render(temp,document.querySelector('#root'));
